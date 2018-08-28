63aff9eecc3b6f1a5d2935293870aa05c4dfbbd3
Atis Elsts <atis.elsts@gmail.com>
2018-08-28 15:19:50 +0100

    exp5438: update config files, Makefile, and remove some unused source files

---
+++ arch/cpu/msp430/cc2420-def.h
@@ -0,0 +1,106 @@
+/**
+ * \file
+ *         Hardware-specific configuration for the CC2420 interface.
+ *
+ *         Platforms using this ile:
+ *         - Tmote Sky
+ *         - the  EXP5438 board
+ */
+
+
+#ifndef CC2420_DEF_H_
+#define CC2420_DEF_H_
+
+/* the low-level radio driver */
+#define NETSTACK_CONF_RADIO   cc2420_driver
+
+
+/* Delay between GO signal and SFD: radio fixed delay + 4Bytes preample + 1B SFD -- 1Byte time is 32us
+ * ~327us + 129preample = 456 us */
+#define RADIO_DELAY_BEFORE_TX ((unsigned)US_TO_RTIMERTICKS(456))
+/* Delay between GO signal and start listening
+ * ~50us delay + 129preample + ?? = 183 us */
+#define RADIO_DELAY_BEFORE_RX ((unsigned)US_TO_RTIMERTICKS(183))
+/* Delay between the SFD finishes arriving and it is detected in software */
+#define RADIO_DELAY_BEFORE_DETECT 0
+
+
+#ifndef CC2420_CONF_SFD_TIMESTAMPS
+#define CC2420_CONF_SFD_TIMESTAMPS (MAC_CONF_WITH_TSCH) /* TSCH needs SFD timestamping */
+#endif /* CC2420_CONF_SFD_TIMESTAMPS */
+
+
+/*
+ * SPI bus - CC2420 pin configuration.
+ */
+
+/* P1.2 - Input: FIFOP from CC2420 */
+#define CC2420_FIFOP_PORT(type)    P1##type
+#define CC2420_FIFOP_PIN           6
+/* P1.3 - Input: FIFO from CC2420 */
+#define CC2420_FIFO_PORT(type)     P1##type
+#define CC2420_FIFO_PIN            5
+/* P1.4 - Input: CCA from CC2420 */
+#define CC2420_CCA_PORT(type)      P1##type
+#define CC2420_CCA_PIN             7
+/* P4.1 - Input:  SFD from CC2420 */
+#define CC2420_SFD_PORT(type)      P1##type
+#define CC2420_SFD_PIN             3
+ /* P3.0 - Output: SPI Chip Select (CS_N) */
+#define CC2420_CSN_PORT(type)      P3##type
+#define CC2420_CSN_PIN             0
+/* P4.5 - Output: VREG_EN to CC2420 */
+#define CC2420_VREG_PORT(type)     P1##type
+#define CC2420_VREG_PIN            4
+/* P4.6 - Output: RESET_N to CC2420 */
+#define CC2420_RESET_PORT(type)    P1##type
+#define CC2420_RESET_PIN           2
+
+
+#define CC2420_IRQ_VECTOR PORT1_VECTOR
+
+/* Pin status. */
+#define CC2420_FIFOP_IS_1 (!!(CC2420_FIFOP_PORT(IN) & BV(CC2420_FIFOP_PIN)))
+#define CC2420_FIFO_IS_1  (!!(CC2420_FIFO_PORT(IN) & BV(CC2420_FIFO_PIN)))
+#define CC2420_CCA_IS_1   (!!(CC2420_CCA_PORT(IN) & BV(CC2420_CCA_PIN)))
+#define CC2420_SFD_IS_1   (!!(CC2420_SFD_PORT(IN) & BV(CC2420_SFD_PIN)))
+
+/* The CC2420 reset pin. */
+#define SET_RESET_INACTIVE()   (CC2420_RESET_PORT(OUT) |=  BV(CC2420_RESET_PIN))
+#define SET_RESET_ACTIVE()     (CC2420_RESET_PORT(OUT) &= ~BV(CC2420_RESET_PIN))
+
+/* CC2420 voltage regulator enable pin. */
+#define SET_VREG_ACTIVE()       (CC2420_VREG_PORT(OUT) |=  BV(CC2420_VREG_PIN))
+#define SET_VREG_INACTIVE()     (CC2420_VREG_PORT(OUT) &= ~BV(CC2420_VREG_PIN))
+
+/* CC2420 rising edge trigger for external interrupt (FIFOP). */
+#define CC2420_FIFOP_INT_INIT() do {                  \
+    CC2420_FIFOP_PORT(IES) &= ~BV(CC2420_FIFOP_PIN);  \
+    CC2420_CLEAR_FIFOP_INT();                         \
+  } while(0)
+
+/* FIFOP on external interrupt 0. */
+#define CC2420_ENABLE_FIFOP_INT()  do {CC2420_FIFOP_PORT(IE) |= BV(CC2420_FIFOP_PIN);} while(0)
+#define CC2420_DISABLE_FIFOP_INT() do {CC2420_FIFOP_PORT(IE) &= ~BV(CC2420_FIFOP_PIN);} while(0)
+#define CC2420_CLEAR_FIFOP_INT()   do {CC2420_FIFOP_PORT(IFG) &= ~BV(CC2420_FIFOP_PIN);} while(0)
+
+
+/*
+ * Enables/disables CC2420 access to the SPI bus (not the bus).
+ * (Chip Select)
+ */
+
+ /* ENABLE CSn (active low) */
+#define CC2420_SPI_ENABLE()     (CC2420_CSN_PORT(OUT) &= ~BV(CC2420_CSN_PIN))
+ /* DISABLE CSn (active low) */
+#define CC2420_SPI_DISABLE()    (CC2420_CSN_PORT(OUT) |=  BV(CC2420_CSN_PIN))
+#define CC2420_SPI_IS_ENABLED() ((CC2420_CSN_PORT(OUT) & BV(CC2420_CSN_PIN)) != BV(CC2420_CSN_PIN))
+
+
+/* Platform-specific (H/W) AES implementation */
+#ifndef AES_128_CONF
+#define AES_128_CONF cc2420_aes_128_driver
+#endif /* AES_128_CONF */
+
+
+#endif /* CC2420_DEF_H_ */
--- arch/platform/exp5438/exp5438-def.h
+++ arch/platform/exp5438/exp5438-def.h
@@ -1,5 +1,87 @@
-// FIXME: The end of stack address is correct, but not sure what else should
-//        go in this header
+/**
+ * \file
+ *         Hardware-specific configuration for EXP5438 board
+ */
+
+#ifndef EXP5438_DEF_H_
+#define EXP5438_DEF_H_
+
+/*
+ * Definitions below are dictated by the hardware and not really changeable.
+ *
+ * Almost all values are the same as for Tmote Sky (e.g. the radio chip is the same: CC2420)
+ */
+
+#define PLATFORM_HAS_LEDS    1
+#define PLATFORM_HAS_RADIO   1
+
+#include "cc2420-def.h"
+
+#define CC2420_CONF_SYMBOL_LOOP_COUNT 1302      /* 326us msp430X @ 8MHz */
+
+/* Disable TSCH frame filtering */
+#define TSCH_CONF_HW_FRAME_FILTERING  0
+
+/* CPU target speed in Hz */
+#define F_CPU 8000000uL /* 8MHz by default */
+
+/* LED ports */
+#define LEDS_PxDIR P1DIR
+#define LEDS_PxOUT P1OUT
+#define LEDS_CONF_RED    0x01
+#define LEDS_CONF_GREEN  0x02
+#define LEDS_CONF_YELLOW 0x02
+
+#define LEDS_CONF_LEGACY_API 1
+
+/* DCO speed resynchronization for more robust UART, etc. */
+/* Not needed from MSP430x5xx since it make use of the FLL */
+#define DCOSYNCH_CONF_ENABLED 0
+#define DCOSYNCH_CONF_PERIOD 30
+
+#define ROM_ERASE_UNIT_SIZE  512
+#define XMEM_ERASE_UNIT_SIZE (64*1024L)
+
+#define CFS_CONF_OFFSET_TYPE    long
+
+/* Use the first 64k of external flash for node configuration */
+#define NODE_ID_XMEM_OFFSET     (0 * XMEM_ERASE_UNIT_SIZE)
+
+/* Use the second 64k of external flash for codeprop. */
+#define EEPROMFS_ADDR_CODEPROP  (1 * XMEM_ERASE_UNIT_SIZE)
+
+#define CFS_XMEM_CONF_OFFSET    (2 * XMEM_ERASE_UNIT_SIZE)
+#define CFS_XMEM_CONF_SIZE      (1 * XMEM_ERASE_UNIT_SIZE)
+
+#define CFS_RAM_CONF_SIZE 4096
+
+/*
+ * SPI bus configuration for the TMote Sky.
+ */
+
+/* SPI input/output registers. */
+#define SPI_TXBUF UCB0TXBUF
+#define SPI_RXBUF UCB0RXBUF
+
+                                /* USART0 Tx ready? */
+#define SPI_WAITFOREOTx() while ((UCB0STAT & UCBUSY) != 0)
+                                /* USART0 Rx ready? */
+#define SPI_WAITFOREORx() while ((UCB0IFG & UCRXIFG) == 0)
+                                /* USART0 Tx buffer ready? */
+#define SPI_WAITFORTxREADY() while ((UCB0IFG & UCTXIFG) == 0)
+
+#define MOSI           1  /* P3.1 - Output: SPI Master out - slave in (MOSI) */
+#define MISO           2  /* P3.2 - Input:  SPI Master in - slave out (MISO) */
+#define SCK            3  /* P3.3 - Output: SPI Serial Clock (SCLK) */
+
+
+/* Use the LCD? */
+#ifndef WITH_LCD
+#define WITH_LCD 0
+#endif
 
 /* Platform-specific define for the end of the stack region */
-#define STACK_CONF_ORIGIN     ((void *)0x4000)
+#undef STACK_CONF_ORIGIN
+#define STACK_CONF_ORIGIN     ((void *)0x5c00)
+
+#endif /* EXP5438_DEF_H_ */
